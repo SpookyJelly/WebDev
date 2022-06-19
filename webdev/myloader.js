bf73179a4463f86216295b5f5f7e9d42a14435bc
SpookyJelly <zang9412@gmail.com>
2022-06-19 23:21:50 +0900

    1.로더 수정

--- webdev/myloader.js
+++ webdev/myloader.js
@@ -1,4 +1,6 @@
 module.exports = function myloader(content) {
   console.log("my loader works!");
-  return content;
+  //   return content
+  return content.replace("console.log(", "alert("); // 로더가 무슨 역할을 하는지, console.log( ) 을 alert으로 변경
+  // main.js는 console.log로 찍혀있는데 serve로 띄운 웹에서는 alert으로 되어있다.
 };
